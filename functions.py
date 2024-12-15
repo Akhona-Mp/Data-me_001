ecd9574d6a4992816f15bdfba247d115b17b2081
m. mouse <akmphuljhb024@student.wethinkcode.co.za>
2024-12-16 00:59:23 +0200

    functions

--- functions.py
+++ functions.py
@@ -17,22 +17,10 @@ def count_occurrences(lst, element):
     :param element: Element to count.
     :return: Integer count of occurrences.
     """
-    occ = []
-    element = []
-    int_count = 0
-    
-    for i in lst:
-        if i not in occ:
-            occ.append(i)
-            
-    for i in occ:
-        for i in lst:
-            if i in occ == i in lst:
-                int_count += 1
-                element.appent(i)
-        return int_count,element
-        
-    pass  # Implement this
+    c = lst.count(element)
+    return c
+
+
 
 def get_keys_with_value(dct, value):
     """
@@ -41,7 +29,8 @@ def get_keys_with_value(dct, value):
     :param value: Value to find.
     :return: List of keys.
     """
-    pass  # Implement this
+    pass
+    # Implement this
 
 def merge_sorted_lists(lst1, lst2):
     """
@@ -52,11 +41,15 @@ def merge_sorted_lists(lst1, lst2):
     """
     new_list = []
     for i in lst1:
-        for i in lst2:
-            new_list.append(i)
-        return new_list.sort()
+        new_list.append(i)
+    for k in lst2:
+        new_list.append(k)
             
-    pass  # Implement this
+    ls = sorted(new_list)
+
+    return ls
+            
+    # Implement this
 
 def find_second_largest(numbers):
     """
@@ -64,8 +57,20 @@ def find_second_largest(numbers):
     :param numbers: List of integers.
     :return: The second largest integer.
     """
-    
-    pass  # Implement this
+    # max1 = max2 = float
+
+    # for i in numbers:
+    #     if i > max1:
+    #         max2 = max1  
+    #         max1 = i  
+    #     elif i > max2 and i != max1:
+    #         max2 = i  
+    #     else:
+    #         return None
+
+    # return max2
+    pass
+    # Implement this
 
 def is_anagram(str1, str2):
     """
@@ -79,11 +84,12 @@ def is_anagram(str1, str2):
     :param str2: Second string.
     :return: True if the strings are anagrams, False otherwise.
     """
-    for i in strt:
-        for p in str2:
-            if i in str1 == p in str2:
-                return True
-    pass  # Implement this
+    for i in str1:
+        if i  in str2:
+            return True
+        else:
+            return False
+     # Implement this
 
 
 def flatten_list(nested_list):
@@ -98,8 +104,8 @@ def flatten_list(nested_list):
     for i in nested_list:
         for n in i:
             flat_list.append(n)
-        return flat_list.sort()
-    pass  # Implement this
+    return flat_list
+    # Implement this
 
 
 def remove_duplicates(lst):
@@ -112,7 +118,7 @@ def remove_duplicates(lst):
     for i in lst:
         if i not in non_dup:
             non_dup.append(i)
-        return non_dup
+    return non_dup
     # Implement this
 
 def find_common_elements(lst1, lst2):
@@ -122,9 +128,10 @@ def find_common_elements(lst1, lst2):
     :param lst2: Second list.
     :return: List of common elements.
     """
+    common = []
     for i in lst1:
-        for i in lst2:
-            if i in lst1 == i in lst2:
-                return i
+        if i in lst2:
+            common.append(i)
+    return common
          
-    pass  # Implement this
+ # Implement this
